23d267f7d4a95f46afb02d4fc6f47f6beba9f10e
Kyle <kyle201817146@gmail.com>
2022-06-28 22:13:17 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kyle on 2022/6/28.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
