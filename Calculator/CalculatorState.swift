ba293ff3ac2a2497c2aedffc8a7444d8d2d6f2e4
Kyle <kyle201817146@gmail.com>
2022-06-29 21:57:16 +0800

    Refactor code and migrate print to oslog

--- Calculator/CalculatorState.swift
+++ Calculator/CalculatorState.swift
@@ -5,12 +5,20 @@
 //  Created by Kyle on 2022/6/28.
 //
 
+import OSLog
 import SwiftUI
 
 class CalculatorState: ObservableObject {
-    enum State {
+    enum State: CustomStringConvertible {
         case value(Int)
         case `operator`(BinaryOperatorKeypad)
+
+        var description: String {
+            switch self {
+            case .value(let number): return "value(\(number))"
+            case .operator(let binaryOperatorKeypad): return "\(type(of: binaryOperatorKeypad))"
+            }
+        }
     }
 
     @Published var state: State = .value(0)
@@ -21,38 +29,19 @@ class CalculatorState: ObservableObject {
 
     private var lastResult: String = ""
 
+    private let logger = Logger(subsystem: "CalculatorState", category: "state")
+
     func updateState(for keypad: some Keypad) {
-        print("[Before] state:\(state) result: \(result) lastResult: \(lastResult)")
-        print("Keypad: \(keypad)")
+        logger.debug("[Before] state:\(self.state) result: \(self.result) lastResult: \(self.lastResult)")
+        logger.debug("Keypad: \(keypad.title)")
         if let keypad = keypad as? NumberInputKeypad {
-            var buffer = ""
-            if case .value = state {
-                buffer = result + keypad.number.description
-            } else if case let .operator(binaryOperatorKeypad) = state {
-                lastResult = result
-                buffer = keypad.number.description
-                lastBinaryOperatorKeypad = binaryOperatorKeypad
-            }
-            buffer = String(buffer.trimmingPrefix("0"))
-            if buffer.hasPrefix(".") || buffer.isEmpty { buffer.insert("0", at: buffer.startIndex) }
-            result = buffer
-            state = .value(keypad.number)
+            numberInput(keypad)
         } else if keypad is DotInputKeypad {
-            if !result.contains(".") {
-                result += "."
-            }
+            dotInput()
         } else if let keypad = keypad as? UnaryOperatorKeypad {
-            result = keypad.operate(result)
+            unaryOperator(keypad)
         } else if let keypad = keypad as? BinaryOperatorKeypad {
-            if case .value = state,
-               let lastBinaryOperatorKeypad,
-               !lastResult.isEmpty
-            {
-                result = lastBinaryOperatorKeypad.operate(lastResult, result)
-                self.lastBinaryOperatorKeypad = nil
-                lastResult = ""
-            }
-            state = .operator(keypad)
+            binaryOperator(keypad)
         } else if keypad is SpecialOperatorKeypad {
             if keypad is EqualOperatorKeypad {
                 equalOperator()
@@ -60,7 +49,44 @@ class CalculatorState: ObservableObject {
                 allClearOperator()
             }
         }
-        print("[After] state: \(state) result: \(result) lastResult: \(lastResult)")
+        logger.debug("[After] state: \(self.state) result: \(self.result) lastResult: \(self.lastResult)")
+    }
+
+    private func numberInput(_ keypad: NumberInputKeypad) {
+        var buffer = ""
+        if case .value = state {
+            buffer = result + keypad.number.description
+        } else if case .operator(let binaryOperatorKeypad) = state {
+            lastResult = result
+            buffer = keypad.number.description
+            lastBinaryOperatorKeypad = binaryOperatorKeypad
+        }
+        buffer = String(buffer.trimmingPrefix("0"))
+        if buffer.hasPrefix(".") || buffer.isEmpty { buffer.insert("0", at: buffer.startIndex) }
+        result = buffer
+        state = .value(keypad.number)
+    }
+
+    private func dotInput() {
+        if !result.contains(".") {
+            result += "."
+        }
+    }
+
+    private func unaryOperator(_ keypad: UnaryOperatorKeypad) {
+        result = keypad.operate(result)
+    }
+
+    private func binaryOperator(_ keypad: BinaryOperatorKeypad) {
+        if case .value = state,
+           let lastBinaryOperatorKeypad,
+           !lastResult.isEmpty
+        {
+            result = lastBinaryOperatorKeypad.operate(lastResult, result)
+            self.lastBinaryOperatorKeypad = nil
+            lastResult = ""
+        }
+        state = .operator(keypad)
     }
 
     private func equalOperator() {
